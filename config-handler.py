89b4e9b1efaffc06bc380380144a71bf7088ea76
School <school@kz.lan>
2022-01-04 15:02:43 -0600

    Added YAML function

--- config-handler.py
+++ config-handler.py
@@ -1,20 +1,24 @@
+import yaml
 import json
 
-"""def process_yaml( file_name ):
+def process_yaml( file_name ):
     yaml_file = open(file_name, "r") 
-    my_config = yaml.load(yaml_file)
+    my_config = yaml.safe_load(yaml_file)
 
     for section in my_config:
         print(section)
-    print(my_config["mysql"])
-    print(my_config["other"])"""
+    print(my_config["oracle"])
+    print(my_config["connections"])
 
 def process_json( file_name ):
     json_file = open(file_name)
     my_config = json.load(json_file)
     print(my_config)
 
+#def process_
+
 if __name__ == "__main__":
 
-    #process_yaml("config_files/test1.yml")
-    process_json("config_files/test2.json")
+    process_yaml("config_files/test1.yml")
+    #process_json("config_files/test2.json")
+    #process_
