2485c40bae45d46d3f311f652b0c0c4e78efca08
School <school@kz.lan>
2022-01-05 20:12:09 -0600

    Added XML config handling

--- config-handler.py
+++ config-handler.py
@@ -1,5 +1,8 @@
 import yaml
 import json
+import configparser as ConfigParser
+import io
+from bs4 import BeautifulSoup
 
 def process_yaml( file_name ):
     yaml_file = open(file_name, "r") 
@@ -15,10 +18,36 @@ def process_json( file_name ):
     my_config = json.load(json_file)
     print(my_config)
 
-#def process_
+"""def process_ini( file_name ):
+    in_file = open(file_name)
+    sample_config = in_file.read()
+    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    config.readfp(io.BytesIO(sample_config))
+
+    # List all contents
+    print("List all contents")
+    for section in config.sections():
+        print("Section: %s" % section)
+        for options in config.options(section):
+            print(
+                "x %s:::%s:::%s"
+                % (options, config.get(section, options), str(type(options)))
+            )
+
+    # Print some contents
+    print("\nPrint some contents")
+    print(config.get("other", "use_anonymous"))  # Just get the value
+    print(config.getboolean("other", "use_anonymous"))"""
+
+def process_xml( file_name ):
+    in_file = open(file_name)
+    my_config = in_file.read()
+    my_result = BeautifulSoup(my_config, "lxml")
+    print(my_result)
 
 if __name__ == "__main__":
 
-    process_yaml("config_files/test1.yml")
+    #process_yaml("config_files/test1.yml")
     #process_json("config_files/test2.json")
-    #process_
+    #process_ini("config_files/test3.ini")
+    process_xml("config_files/test4.xml")
